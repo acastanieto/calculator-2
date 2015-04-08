36c3d54cb2e01953549d531b880b26efcb261fa7
Hackbright Student <info@hackbrightacademy.com>
2015-04-08 16:15:07 -0700

    All done

--- calculator.py
+++ calculator.py
@@ -26,7 +26,20 @@ def calculator_2(string):
         print(multiply(int(tokens[1]),int(tokens[2])))    
 
     elif tokens[0] == "/":
-        print(divide(int(tokens[1]),int(tokens[2])))     
+        print(divide(int(tokens[1]),int(tokens[2])))
+    
+    elif tokens[0] == "square":
+        print(square(int(tokens[1])))
+
+    elif tokens[0] == "cube":
+        print(cube(int(tokens[1])))
+
+    elif tokens[0] == "pow":
+        print(power(int(tokens[1]),int(tokens[2])))       
+
+    elif tokens[0] == "mod":
+        print(divide(int(tokens[1]),int(tokens[2]))) 
+
 
 
 
@@ -35,3 +48,7 @@ calculator_2("- 2 3")
 calculator_2("* 2 3")
 calculator_2("/ 2 3")
 calculator_2("q")
+calculator_2("mod 10 3")
+calculator_2("pow 2 5")
+calculator_2("cube 3")
+calculator_2("square 2")
