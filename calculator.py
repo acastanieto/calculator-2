efa627dcd31d38f4afb6267d2eb8d8039be16abf
Hackbright Student <info@hackbrightacademy.com>
2015-04-08 16:11:04 -0700

    Added more operator functions

--- calculator.py
+++ calculator.py
@@ -10,11 +10,28 @@ from arithmetic import *
 
 # Your code goes here
 def calculator_2(string):
+
     tokens = string.split(" ")
+
     if tokens[0] == "q":
         return
-    elif tokens[0] == "add":
+
+    elif tokens[0] == "+":
         print(add(int(tokens[1]),int(tokens[2])))
 
-calculator_2("add 2 3")
+    elif tokens[0] == "-":
+        print(subtract(int(tokens[1]),int(tokens[2])))
 
+    elif tokens[0] == "*":
+        print(multiply(int(tokens[1]),int(tokens[2])))    
+
+    elif tokens[0] == "/":
+        print(divide(int(tokens[1]),int(tokens[2])))     
+
+
+
+calculator_2("+ 2 3")
+calculator_2("- 2 3")
+calculator_2("* 2 3")
+calculator_2("/ 2 3")
+calculator_2("q")
