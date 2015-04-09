8d85b67214337b4beef68b9ec7f847289079052e
Hackbright Student <info@hackbrightacademy.com>
2015-04-08 18:14:36 -0700

    Finished

--- calculator.py
+++ calculator.py
@@ -12,52 +12,52 @@ from arithmetic import *
 def calculator_2(string):
 
     tokens = string.split(" ")
-    arguments = tokens[1:]
+    arguments = tokens[2:]
+    running_total = int(tokens[1])
 
     if tokens[0] == "q":
         return
 
     elif tokens[0] == "+":
-        running_total = 0
         for i in range(len(arguments)):
             running_total = add(running_total, int(arguments[i]))
         print running_total
 
     elif tokens[0] == "-":
-        running_total = 0
         for i in range(len(arguments)):
             running_total = subtract(running_total, int(arguments[i]))
         print running_total
 
     elif tokens[0] == "*":
-        running_total = 0
         for i in range(len(arguments)):
             running_total = multiply(running_total, int(arguments[i]))
         print running_total
 
     elif tokens[0] == "/":
-        running_total = 0
         for i in range(len(arguments)):
-            running_total = divide(running_total, int(arguments[i]))
+            running_total = divide(running_total, float(arguments[i]))
         print running_total
     
     elif tokens[0] == "square":
-        print(square(int(tokens[1])))
+        print square(int(tokens[1]))
 
     elif tokens[0] == "cube":
-        print(cube(int(tokens[1])))
+        print cube(int(tokens[1]))
 
     elif tokens[0] == "pow":
-        running_total = 0
         for i in range(len(arguments)):
-            running_total = power(running_total, int(arguments[i]))
-        print running_total      
-
+            running_total = power(running_total, float(arguments[i]))
+        print running_total
+    
     elif tokens[0] == "mod":
-        print(divide(int(tokens[1]),int(tokens[2]))) 
+        print mod(int(tokens[1]),int(tokens[2]))
 
 
 
 
-calculator_2("pow 2 2 2")
+calculator_2("- 10 2 2") # 6
+calculator_2("pow 2 2 2") # 16
+calculator_2("/ 10 2 2") # 2.5
+
+
 
